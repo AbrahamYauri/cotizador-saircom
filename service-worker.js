ed868b87387707af409a547b8ab793ec357881a1
Abraham_Yauri <105977411+AbrahamYauri@users.noreply.github.com>
2026-06-01 23:38:47 -0500

    Update service-worker.js
    El sistema detecta si hay una version nueva y actualiza si hay conexión a internet

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "cotizador-saircom-v1";
+const CACHE_NAME = "cotizador-saircom-v2";
 
 const APP_FILES = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
+  "./service-worker.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./libs/html2canvas.min.js",
@@ -11,31 +12,50 @@ const APP_FILES = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
-  );
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    }).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put("./index.html", responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
+    caches.match(request).then(cachedResponse => {
+      return cachedResponse || fetch(request);
     })
   );
 });
